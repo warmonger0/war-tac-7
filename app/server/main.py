144f0ef6e29376fbf95cd5a3a82d698eaade83b3
warmonger0 <spanospr@gmail.com>
2025-11-10 09:28:49 -0800

    test_runner: chore: fix server entry point reference

--- app/server/main.py
+++ app/server/main.py
@@ -1,5 +1,3 @@
-from server import app
-
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8002, reload=True)
